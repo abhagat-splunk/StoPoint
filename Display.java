ec1ae65138e92eebeaeba98e0ed978104989bbc8
xynazog <ankit.bhagat@live.com>
2014-09-23 00:13:55 +0530

    Create Display.java

---
+++ Display.java
@@ -0,0 +1,21 @@
+package com.example.stoppointui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Display extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.displayres);
+	}
+
+
+}
