e134d9c2ef2c5bcdbd862afcb996a4fcb7b25ebd
xynazog <ankit.bhagat@live.com>
2014-09-23 00:13:27 +0530

    Create About.java

---
+++ About.java
@@ -0,0 +1,19 @@
+package com.example.stoppointui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity{
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+	
+
+}
